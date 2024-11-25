5fc95f22598a18b4f78814eb3cc6ce53ccc26fbf
ferid aqa <feridcodee@gmail.com>
2024-11-25 16:20:07 +0400

    register elave etdim

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,10 @@
 const express =require('express')
 const loginLimiter = require('../middleware/loginLimiter')
 const { c_register_user, c_login } = require('../controller/auth_contr')
+const registerValidation = require('../middleware/registerValidation')
 const router=express.Router()
 
-router.post('/register',c_register_user)
+router.post('/register',registerValidation,c_register_user)
 router.post('/login',loginLimiter,c_login)
 
 
